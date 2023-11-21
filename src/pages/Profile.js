1116d47d8105f28a857427e62dd0d9d334232ebf
MukeshPrasadYadav <my699546@gmail.com>
2023-11-22 00:49:21 +0530

    profile changed

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import classes from './Profile.module.css'
+import profilepic from '../Assets/profile.jpg'
+import { FaFacebook } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import { FaHome } from 'react-icons/fa'
 const Profile = () => {
   return (
-    <div>Profile</div>
+   <div className={classes.container}>
+   
+    <div className={classes['profile-box']}>
+      <img src={profilepic} className={classes['profile-pic']} />
+      <a href='./Home' className={classes['home-pic']}><FaHome></FaHome></a>
+      <h3 className={classes.name}>Jhonny Bhaiya</h3>
+      <p className={classes.des}> International khiladi</p>
+      <div className={classes['profile-lower']}>
+      <a href='https://www.facebook.com/JoSinss'><FaFacebook></FaFacebook></a>
+      <a href='https://www.instagram.com/jhony_.sins._/'><FaInstagram></FaInstagram></a>
+      </div>
+    </div>
+   </div>
   )
 }
 
