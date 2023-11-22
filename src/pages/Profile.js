017d3b14d9a9e64ac02d599f10aa7c86079ec6ac
Vis7044 <singhvishal7044@gmail.com>
2023-11-22 17:59:24 +0530

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from "react";
 
-import classes from './Profile.module.css'
-import profilepic from '../Assets/profile.jpg'
-import { FaFacebook } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
-import { FaTwitter } from 'react-icons/fa'
-import { FaHome } from 'react-icons/fa'
-
+import classes from "./Profile.module.css";
+import profilepic from "../Assets/profile.jpg";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 
 const Profile = () => {
   return (
-   <div className={classes['container-area']}>
-        <div className={classes['profile-box']}>
-      <div className={classes['img-area']}>
-        <img src={profilepic} />
-      </div>  
-      <div className={classes['text-area']}>
-        <p className={classes['name']}>Jhonny Bhaiya</p>
-        <p className={classes['job']}>Aurto ka masiha</p>
-        <p>Age:40 yrs</p>
-        <p>Girl-Friend: Vishal ki Maugi</p>
+    <div className={classes["container-area"]}>
+      <div className={classes["profile-box"]}>
+        <div className={classes["img-area"]}>
+          <img src={profilepic} />
+        </div>
+        <div className={classes["text-area"]}>
+          <p className={classes["name"]}>Jhonny Bhaiya</p>
+          <p className={classes["job"]}>Aurto ka masiha</p>
+          <p>Age:40 yrs</p>
+          <p>Girl-Friend: Vishal ki Maugi</p>
+        </div>
+        <div className={classes["button-area"]}>
+          <button className={classes["follow-button"]}>Log Out</button>
+        </div>
+        <div className={classes["media-button"]}>
+          <a className={classes["link-area"]}>
+            <FaFacebook></FaFacebook>
+          </a>
+          <a id="one" className={classes["link-area"]}>
+            <FaInstagram></FaInstagram>
+          </a>
+          <a className={classes["link-area"]}>
+            <FaTwitter></FaTwitter>
+          </a>
+        </div>
       </div>
-      <div className={classes['button-area']}>
-        <button className={classes['follow-button']}>Log Out</button>
-      </div>
-      <div className={classes['media-button']}>
-        <a href='' className={classes['link-area']}><FaFacebook></FaFacebook></a>
-        <a href=''id='one' className={classes['link-area'] }><FaInstagram></FaInstagram></a>
-        <a href='' className={classes['link-area']}><FaTwitter></FaTwitter></a>
-      </div>
-      
     </div>
-   </div>
+  );
+};
 
-  )
-}
-
-export default Profile
+export default Profile;
