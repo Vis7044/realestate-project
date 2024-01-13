61fa76b70b9c1ccd37d8cd1e2776c05a43ec6848
Vis7044 <singhvishal7044@gmail.com>
2024-01-13 15:10:32 +0530

    firebase image upload

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,20 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHFdD514m9fKklhdKPt_pdMcuaJ8FQvQs",
-  authDomain: "mern-estate-9cc81.firebaseapp.com",
-  projectId: "mern-estate-9cc81",
-  storageBucket: "mern-estate-9cc81.appspot.com",
-  messagingSenderId: "955512893227",
-  appId: "1:955512893227:web:a21caac348b79f4489df6e"
+  apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
+  authDomain: "mern-estate-3284a.firebaseapp.com",
+  projectId: "mern-estate-3284a",
+  storageBucket: "mern-estate-3284a.appspot.com",
+  messagingSenderId: "788297806538",
+  appId: "1:788297806538:web:9f861f269eba84cb4dba33"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
