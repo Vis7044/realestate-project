dc32325dca44a4866ce54cf94a01555ead9dde5f
MukeshPrasadYadav <my699546@gmail.com>
2024-01-13 11:46:17 +0530

    Profile page done

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 export default function Profile(){
   const {currentUser}=useSelector((state)=>state.user)
 console.log(currentUser)
+const [changeEmail,setChangeEmail]=useState(currentUser.email)
+
+const [changeUsername,setChangeUsername]=useState(currentUser.username)
+const [changePassword,setChangePassword]=useState("")
+
   return(
-    <div className="p-3 max-w-lg mx-auto">
+    <div className="  p-3 max-w-lg mx-auto ">
     <h1 className="text-3xl font-semibold text-center my-7 text-black">Profile</h1>
     <form className="flex flex-col gap-5">
       <img src={currentUser.avatar} alt="Profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center" />
 
-      <input placeholder={currentUser.username} type="text" className="bordr p-3 rounded-lg" id="username" />
+      <input placeholder={currentUser.username} value={currentUser.username} type="text" className="bordr p-3 rounded-lg" id="username" onChange={setChangeUsername} />
       
-      <input placeholder={currentUser.email} type="email" className="bordr p-3 rounded-lg" id="email" />
+      <input placeholder={currentUser.email} type="email" value={currentUser.email} className="bordr p-3 rounded-lg" id="email" onClick={setChangeEmail} />
 
-      <input placeholder="password" type="text" className="bordr p-3 rounded-lg" id="password" />
+      <input placeholder="password" type="password" className="bordr p-3 rounded-lg" id="password"  />
       <button className="bg-slate-700 text-white roundedl-lg p-3 uppercase hover:opacity-95 disabled: opacity-80">Update</button>
   
     </form>
