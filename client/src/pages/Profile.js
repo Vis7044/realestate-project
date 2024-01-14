5f6d0ede14d7053261efa8bc733dab53fed91e98
Vis7044 <singhvishal7044@gmail.com>
2024-01-14 13:17:36 +0530

    add signout functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
@@ -112,6 +115,23 @@ export default function Profile() {
     }
   };
 
+  const handleSignout = async () => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json()
+      if(data.succes === false) {
+        dispatch(signOutUserFailure(data.message))
+        return;
+      }
+      dispatch(signOutUserSuccess(data))
+
+    } catch (error) {
+      dispatch(signOutUserFailure(error.message))
+      
+    }
+  }
+
   return (
     <div className="  p-3 max-w-lg mx-auto ">
       <h1 className="text-3xl font-semibold text-center my-7 text-black">
@@ -186,7 +206,7 @@ export default function Profile() {
           Delete Account
         </span>
 
-        <span className="text-red-700 cursor-pointer"> Sign Out</span>
+        <span className="text-red-700 cursor-pointer" onClick={handleSignout}> Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
